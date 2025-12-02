ce7cd87262bde16afadd0bc65c8b48ea697cff55
Justin Geeslin <justin@geesl.in>
2025-12-01 23:12:37 -0600

    Working packaide serverless on runpod worker

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,38 @@
 # Include Python
-FROM python:3.10-alpine
+FROM python:3.12-slim
 
 # Label your image with metadata
 LABEL maintainer="info@blib.la"
 LABEL org.opencontainers.image.source https://github.com/blib-la/runpod-worker-helloworld
 LABEL org.opencontainers.image.description "Getting started with a serverless endpoint on RunPod by creating a custom worker"
 
-# Define your working directory
 WORKDIR /
-
-# Copy the requirements into the image
-COPY requirements.txt ./
-
-# Intall dependencies
-RUN apk add --no-cache bash gcc libc-dev libffi-dev && \
-    pip3 install --no-cache-dir -r requirements.txt
 
 # Copy your source code into the image
 COPY src/ .
 
 # Make the start script executable
 RUN chmod +x start.sh
+
+# Define your working directory
+WORKDIR /app
+
+# Copy the requirements into the image
+COPY requirements.txt .
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential gcc g++ cmake git libcgal-dev libboost-all-dev pkg-config
+RUN pip install --no-cache-dir -r requirements.txt gunicorn parameterized shapely svgelements
+
+RUN git clone https://github.com/DanielLiamAnderson/Packaide.git
+WORKDIR /app/Packaide
+RUN mkdir -p build \
+ && cmake -S /app/Packaide/ -B build  \
+ && cmake --build build -j$(nproc) \
+ && cmake --install build
+COPY . .
+
+WORKDIR /
 
 # Run the start script
 CMD ["/start.sh"]
--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,11 +1,54 @@
-import runpod
+import sys
+import os
+packaide_path = os.path.join('/app', 'Packaide', 'python')
+if packaide_path not in sys.path:
+    sys.path.append(packaide_path)
 
+import packaide
+
+import runpod
 
 def hello_world(greeting):
     if not isinstance(greeting, str):
         return {"error": "Please provide a String"}
 
-    return f"Hello {greeting}"
+        # Example usage (from your previous cell)
+        # Shapes are provided in SVG format
+    shapes = """
+       <svg viewBox="0 0 432.13 593.04">
+         <rect width="100" height="50" />
+         <rect width="50" height="100" />
+         <ellipse rx="20" ry="20" />
+       </svg>
+       """
+
+    # The target sheet / material is also represented as an SVG
+    # document. Shapes given on the sheet are interpreted as
+    # holes that must be avoided when placing new parts. In this
+    # case, a square in the upper-left-hand corner.
+    sheet = """
+       <svg width="300" height="300" viewBox="0 0 300 300">
+         <rect x="0" y="0" width="100" height="100" />
+       </svg>
+       """
+
+    # Attempts to pack as many of the parts as possible.
+    result, placed, fails = packaide.pack(
+        [sheet],  # A list of sheets (SVG documents)
+        shapes,  # An SVG document containing the parts
+        tolerance=2.5,  # Discretization tolerance
+        offset=5,  # The offset distance around each shape (dilation)
+        partial_solution=True,  # Whether to return a partial solution
+        rotations=1,  # The number of rotations of parts to try
+        persist=True  # Cache results to speed up next run
+    )
+
+    # If partial_solution was False, then either every part is placed or none
+    # are. Otherwise, as many as possible are placed. placed and fails denote
+    # the number of parts that could be and could not be placed respectively
+    return "{} parts were placed. {} parts could not fit on the sheets".format(placed, fails)
+
+    # return "Hello from Flask in a container!"
 
 
 def handler(job):
