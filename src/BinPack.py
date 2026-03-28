8cc3bc20a7443398f339bb5e7e186b6f6f9d8b1f
Justin Geeslin <justin@geesl.in>
2026-03-28 14:09:28 -0500

    Irregular stock first draft, passing tests

--- src/BinPack.py
+++ src/BinPack.py
@@ -18,6 +18,49 @@ class BinPack:
            </svg>
         """
 
+    def _detect_irregular_stock(self):
+        # Assume its all irregular for now.
+        return True
+
+    def make_irregular_stock_then_pack(self):
+        if self._detect_irregular_stock():
+            # Doesn't return just sets a new self.bin
+            self._make_irregular_stock_with_packed_holes()
+
+        return self.pack()
+
+    def _make_irregular_stock_with_packed_holes(self):
+        holes = """
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 width="135000px" height="142800px"
+                 viewBox="0.00 0.00 135000.00 142800.00">"""
+
+        # todo scale to match the bin size.
+        holeWidth = 50
+        numberOfHoles = 400
+
+        for i in range(numberOfHoles):
+            holes = holes + f'<rect width="{holeWidth}" height="{holeWidth}" />'
+
+        holes = holes + "</svg>"
+
+        # Pack holds around the bin
+        result, placed, fails = packaide.pack(
+            [self.bin],
+            holes,
+            tolerance=2.5,  # Discretization tolerance
+            offset=0,  # The offset distance around each shape (dilation)
+            partial_solution=True,  # Whether to return a partial solution
+            rotations=1,  # The number of rotations of parts to try
+            persist=False  # Cache results to speed up next run
+        )
+
+        print("DEBUG: This is the hole-y bin:")
+        print(result[0][1])
+
+        # The result is the new bin with packed shapes interpreted as "holes" by packaide during self.pack()
+        self.bin = result[0][1]
+
     def pack(self):
         if not isinstance(self.parts, str):
             return {"error": "Please provide at least one part."}
@@ -34,13 +77,18 @@ class BinPack:
             persist=True  # Cache results to speed up next run
         )
 
-        # If partial_solution was False, then either every part is placed or none
-        # are. Otherwise, as many as possible are placed. placed and fails denote
-        # the number of parts that could be and could not be placed respectively
-        return {
-            "result": result,
+        result_object = {
+            "result": result[0][1],
             "placed": placed,
             "fails": fails,
             "total": placed + fails,
             "description": "{} parts were placed. {} parts could not fit on the sheets".format(placed, fails),
         }
+
+        print(result_object)
+        print("DEBUG: Description" + str(result_object["description"]))
+
+        # If partial_solution was False, then either every part is placed or none
+        # are. Otherwise, as many as possible are placed. placed and fails denote
+        # the number of parts that could be and could not be placed respectively
+        return result_object
--- src/rp_handler.py
+++ src/rp_handler.py
@@ -6,12 +6,18 @@ import runpod
 
 def handler(job):
     myBinPack = BinPack()
+
     myBinPack.parts = job["input"]['parts']
 
-    if job["input"]['bin']:
+    if 'bin' in job["input"]:
+        # When a custom bin is supplied, apply it and make irregular stock before packing
         myBinPack.bin = job["input"]['bin']
+        result = myBinPack.make_irregular_stock_then_pack()
+    else:
+        # If not bin is supplied us the default one and pack
+        result = myBinPack.pack()
 
-    return myBinPack.pack()
+    return result
 
 
 # Start the handler only if this script is run directly
